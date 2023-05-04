6d62c323bcb3f10885d1123be1b92fe0d35a481e
Precious Abubakar <preciousdanabubakar@gmail.com>
2023-05-04 23:43:36 +0100

    create SVG components for the "how it works" icons

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -9,14 +9,21 @@ import discord from "./logos/discord_logo.svg";
 import dropbox from "./logos/dropbox_logo.svg";
 import badgingLogo from "./logos/all-in-chaoss-logo.svg";
 
+import ApplySvgComponent from "./icons/Apply.js";
+import ReviewSvgComponent from "./icons/Review.js";
+import GetBadgedSvgComponent from "./icons/GetBadged.js";
+
 export {
-  badgingLogo,
-  twitter,
-  reddit,
-  linkedin,
-  mail,
-  loom,
-  github,
-  discord,
-  dropbox,
+	badgingLogo,
+	twitter,
+	reddit,
+	linkedin,
+	mail,
+	loom,
+	github,
+	discord,
+	dropbox,
+	ApplySvgComponent,
+	ReviewSvgComponent,
+	GetBadgedSvgComponent,
 };
