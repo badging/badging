67887367fa82103fc1331aabcf67f3403899bfdb
Tochukwu <tochukwuodoeme@gmail.com>
2023-05-05 16:02:02 +0100

    add table filter and search button

--- src/pages/Projects/Projects.js
+++ src/pages/Projects/Projects.js
@@ -11,11 +11,30 @@ const Projects = () => {
       <Jumbotron />
         <section >
             <div className="badging">
-              <div className="filter">
-                Filter
+              <div className="search">
+                <button className='filter'>M Filter</button>
+                <input type="input" className='filter-input' />
+                <button className='search-button'>Search</button>
               </div>
               <div className="badging-table">
-                Badging Table
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Hello</th>
+                      <th>Hello</th>
+                      <th>Hello</th>
+                      <th>Hello</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>jsdkdkj</td>
+                      <td>jsdkdkj</td>
+                      <td>jsdkdkj</td>
+                      <td>jsdkdkj</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
               <div className="badging-table">
                Paginate
