cdf4af458d3bf7f9dfe2843a487fb5e7fa6add17
Tochukwu <tochukwuodoeme@gmail.com>
2023-05-05 11:39:35 +0100

    add reuseable component (Jumbotron) to projects page

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Jumbotron = () => {
+  return (
+    
+        <div className="jumbotron">
+          <h2>DEI Badged Projects</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Sed risus ultrices sit nibh sed. 
+            Interdum urna molestie fames porttitor elementum a diam laoreet. Sed 
+            tempor habitant phasellus velit sagittis mauris lorem pretium. 
+            Arcu neque id duis eu pellentesque in amet et. Ipsum ultricies 
+            a etiam est. Viverra eleifend tortor iaculis fringilla sed.
+          </p>
+        </div>
+      
+  )
+}
+
+export default Jumbotron
