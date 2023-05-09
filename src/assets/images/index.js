3e2a5f14eddc50512bb475c7e904a4d4a5ebcd01
Precious Abubakar <preciousdanabubakar@gmail.com>
2023-05-09 15:39:32 +0100

    [feat] About DEI Badging Page: -main section only - designed for mobile screens only

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -2,6 +2,9 @@ import twitter from "./icons/twitter.svg";
 import reddit from "./icons/reddit.svg";
 import linkedin from "./icons/linkedin.svg";
 import mail from "./icons/mail.svg";
+import ApplySvgComponent from "./icons/Apply.js";
+import ReviewSvgComponent from "./icons/Review.js";
+import GetBadgedSvgComponent from "./icons/GetBadged.js";
 
 import github from "./logos/github_logo.svg";
 import loom from "./logos/loom_logo.svg";
@@ -9,9 +12,9 @@ import discord from "./logos/discord_logo.svg";
 import dropbox from "./logos/dropbox_logo.svg";
 import badgingLogo from "./logos/all-in-chaoss-logo.svg";
 
-import ApplySvgComponent from "./icons/Apply.js";
-import ReviewSvgComponent from "./icons/Review.js";
-import GetBadgedSvgComponent from "./icons/GetBadged.js";
+import goldBadge from "./others/gold-badge.svg";
+import silverBadge from "./others/silver-badge.svg";
+import passingBadge from "./others/passing-badge.svg";
 
 export {
 	badgingLogo,
@@ -19,11 +22,14 @@ export {
 	reddit,
 	linkedin,
 	mail,
+  ApplySvgComponent,
+	ReviewSvgComponent,
+	GetBadgedSvgComponent,
 	loom,
 	github,
 	discord,
 	dropbox,
-	ApplySvgComponent,
-	ReviewSvgComponent,
-	GetBadgedSvgComponent,
+  goldBadge,
+  silverBadge,
+  passingBadge
 };
---
+++ src/pages/About/About.js
@@ -0,0 +1,72 @@
+import "../../assets/styles/global.scss";
+import "./about.scss";
+import { Footer, Header } from "../../components";
+import { goldBadge, silverBadge, passingBadge } from "../../assets/images";
+
+const About = () => {
+	return (
+		<div className="about-dei">
+			<Header />
+			<main>
+				<section className="top">
+					<div>
+						<h2>What is DEI Project Badging?</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+							molestie molestie massa. Odio non curabitur in quam ullamcorper
+							tellus quam augue cum. Neque ut
+						</p>
+					</div>
+					<div>
+						<h2>DEI Project Badging Objective</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+							molestie molestie massa. Odio non curabitur in quam ullamcorper
+							tellus quam augue cum. Neque ut
+						</p>
+					</div>
+				</section>
+				<section className="btm">
+					<div>
+						<h2>Application Process</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+							molestie molestie massa. Odio non curabitur in quam ullamcorper
+							tellus quam augue cum. Neque ut
+						</p>
+					</div>
+					<div className="dei-file">
+						<h2>DEI.md File</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+							molestie molestie massa. Odio non curabitur in quam ullamcorper
+							tellus quam augue cum. Neque ut
+						</p>
+						<img src="https://via.placeholder.com/150" alt="DEI.md file" />
+					</div>
+					<div className="dei-badge">
+						<h2>DEI Badge</h2>
+						<p>
+							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+							molestie molestie massa. Odio non curabitur in quam ullamcorper
+							tellus quam augue cum. Neque ut
+						</p>
+						<div className="badges">
+							<img src={goldBadge} alt="Gold badge" />
+							<img src={passingBadge} alt="Passing badge" />
+							<img src={silverBadge} alt="Silver badge" />
+						</div>
+					</div>
+				</section>
+			</main>
+			<Footer />
+		</div>
+	);
+};
+
+export default About;
