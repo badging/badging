0027aa31914085df5deaeeb2a2a146bbbeef2c82
Precious Abubakar <preciousdanabubakar@gmail.com>
2023-05-09 17:39:08 +0100

    fix: optimise design for larger screens

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -9,57 +9,61 @@ const About = () => {
 			<Header />
 			<main>
 				<section className="top">
-					<div>
-						<h2>What is DEI Project Badging?</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
-							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
-							molestie molestie massa. Odio non curabitur in quam ullamcorper
-							tellus quam augue cum. Neque ut
-						</p>
-					</div>
-					<div>
-						<h2>DEI Project Badging Objective</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
-							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
-							molestie molestie massa. Odio non curabitur in quam ullamcorper
-							tellus quam augue cum. Neque ut
-						</p>
+					<div className="content">
+						<div>
+							<h2>What is DEI Project Badging?</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+								ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+								molestie molestie massa. Odio non curabitur in quam ullamcorper
+								tellus quam augue cum. Neque ut
+							</p>
+						</div>
+						<div>
+							<h2>DEI Project Badging Objective</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+								ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+								molestie molestie massa. Odio non curabitur in quam ullamcorper
+								tellus quam augue cum. Neque ut
+							</p>
+						</div>
 					</div>
 				</section>
 				<section className="btm">
-					<div>
-						<h2>Application Process</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
-							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
-							molestie molestie massa. Odio non curabitur in quam ullamcorper
-							tellus quam augue cum. Neque ut
-						</p>
-					</div>
-					<div className="dei-file">
-						<h2>DEI.md File</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
-							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
-							molestie molestie massa. Odio non curabitur in quam ullamcorper
-							tellus quam augue cum. Neque ut
-						</p>
-						<img src="https://via.placeholder.com/150" alt="DEI.md file" />
-					</div>
-					<div className="dei-badge">
-						<h2>DEI Badge</h2>
-						<p>
-							Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
-							ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
-							molestie molestie massa. Odio non curabitur in quam ullamcorper
-							tellus quam augue cum. Neque ut
-						</p>
-						<div className="badges">
-							<img src={goldBadge} alt="Gold badge" />
-							<img src={passingBadge} alt="Passing badge" />
-							<img src={silverBadge} alt="Silver badge" />
+					<div className="content">
+						<div>
+							<h2>Application Process</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+								ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+								molestie molestie massa. Odio non curabitur in quam ullamcorper
+								tellus quam augue cum. Neque ut
+							</p>
+						</div>
+						<div className="dei-file ">
+							<h2>DEI.md File</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+								ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+								molestie molestie massa. Odio non curabitur in quam ullamcorper
+								tellus quam augue cum. Neque ut
+							</p>
+							<img src="https://via.placeholder.com/150" alt="DEI.md file" />
+						</div>
+						<div className="dei-badge ">
+							<h2>DEI Badge</h2>
+							<p>
+								Lorem ipsum dolor sit amet consectetur. Sit sed aliquam
+								ullamcorper feugiat ut phasellus nunc. Morbi feugiat tempus
+								molestie molestie massa. Odio non curabitur in quam ullamcorper
+								tellus quam augue cum. Neque ut
+							</p>
+							<div className="badges">
+								<img src={goldBadge} alt="Gold badge" />
+								<img src={passingBadge} alt="Passing badge" />
+								<img src={silverBadge} alt="Silver badge" />
+							</div>
 						</div>
 					</div>
 				</section>
