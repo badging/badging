3766f0263bfe5387616cefbc903307c2259c80c7
mide358 <iyimide.adegunloye@gmail.com>
2023-05-11 22:02:30 +0100

    adjusted file structure

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,5 @@ import Footer from './Footer/Footer';
 import Faq from './Faq/Faq';
 import CardComponent from './HowItWorks/Card';
 import CardCarousel from './Carousel/CardCarousel';
-import BadgingForm from './BadgingForm/BadgingForm';
-export { Header, Footer, Faq, CardComponent, CardCarousel, BadgingForm };
+
+export { Header, Footer, Faq, CardComponent, CardCarousel };
