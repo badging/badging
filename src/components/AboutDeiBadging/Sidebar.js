5568992dbf6c4a6b40240ab5c0126e03811db32e
Precious Abubakar <preciousdanabubakar@gmail.com>
2023-07-16 15:15:44 +0100

    refactor: create component for mobile view

--- src/components/AboutDeiBadging/Sidebar.js
+++ src/components/AboutDeiBadging/Sidebar.js
@@ -1,20 +1,18 @@
-import React from "react";
-
 const Sidebar = () => {
 	return (
 		<ul>
 			<li>
 				<a href="#what-is-dei-project-badging">What is DEI Project Badging?</a>
 			</li>
-      <li>
+			<li>
 				<a href="#application-process">Application Process</a>
 			</li>
-      <li>
-        <a href="#dei-file">DEI.md File</a>
-      </li>
-      <li>
-        <a href="#dei-badges">DEI Badges</a>
-      </li>
+			<li>
+				<a href="#dei-file">DEI.md File</a>
+			</li>
+			<li>
+				<a href="#dei-badges">DEI Badges</a>
+			</li>
 		</ul>
 	);
 };
--- src/components/index.js
+++ src/components/index.js
@@ -4,7 +4,8 @@ import Faq from './Faq/Faq';
 import CardComponent from './HowItWorks/Card';
 import CardCarousel from './Carousel/CardCarousel';
 import SearchBar from './SearchBar/SearchBar';
-import Sidebar from './About/Sidebar';
+import Sidebar from './AboutDeiBadging/Sidebar';
 import Jumbotron from './Jumbotron/Jumbotron';
+import AboutDeiMobile from './AboutDeiBadging/AboutDeiMobile';
 
-export { Header, Footer, Faq, CardComponent, CardCarousel, Jumbotron, SearchBar, Sidebar };
+export { Header, Footer, Faq, CardComponent, CardCarousel, Jumbotron, SearchBar, Sidebar, AboutDeiMobile };
