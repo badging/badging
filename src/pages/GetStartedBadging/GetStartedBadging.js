2a9e6e260eef458ef983797fceef7baf246e1c27
mide358 <iyimide.adegunloye@gmail.com>
2023-09-15 12:39:00 +0100

    Get started badging page redesing

--- src/pages/GetStartedBadging/GetStartedBadging.js
+++ src/pages/GetStartedBadging/GetStartedBadging.js
@@ -13,39 +13,36 @@ const GetStartedBadging = () => {
   };
   return (
     <Layout>
-      <div className="">
-        <div className="section-two">
-          <div className="section-wrap">
-            <div className="section-title">
-              <h2>Prerequisite</h2>
-              <hr />
-            </div>
-            <ol className="section-list">
-              <li>
-                A repository is required within which to put the DEI.md file
-              </li>
-              <li>
-                The person applying for the badge must be a project admin or
-                maintainer
-              </li>
+      <div className="section-two">
+        <div className="section-wrap">
+          <div className="section-title">
+            <h2>Prerequisite</h2>
+          </div>
+          <ol className="section-list">
+            <li>
+              A repository is required within which to put the DEI.md file
+            </li>
+            <li>
+              The person applying for the badge must be a project admin or
+              maintainer
+            </li>
 
-              <li>The Project must be Open Source</li>
-              <li>Ensure you have your DEI.md File</li>
-            </ol>
+            <li>The Project must be Open Source</li>
+            <li>Ensure you have your DEI.md File</li>
+          </ol>
 
-            <div className="login-github">
-              <button
-                type="button"
-                className="login-github"
-                onClick={handleLogin}
-              >
-                <span className="btn-mb-badge">Get Started</span>{' '}
-                <span className="btn-dt-badge"> Login with GitHub</span>
-                <span>
-                  <img className="arrow" src={loginArrow} alt="arrow icon" />
-                </span>
-              </button>
-            </div>
+          <div className="login-github">
+            <button
+              type="button"
+              className="login-github"
+              onClick={handleLogin}
+            >
+              <span className="btn-mb-badge">Get Started</span>{' '}
+              <span className="btn-dt-badge"> Login with GitHub</span>
+              <span>
+                <img className="arrow" src={loginArrow} alt="arrow icon" />
+              </span>
+            </button>
           </div>
         </div>
       </div>
