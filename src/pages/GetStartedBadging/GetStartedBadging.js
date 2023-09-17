676eef320f04de5685241de463266c0de763a250
Precious Abubakar <preciousdanabubakar@gmail.com>
2023-09-18 00:53:44 +0100

    feat: add loader

--- src/pages/GetStartedBadging/GetStartedBadging.js
+++ src/pages/GetStartedBadging/GetStartedBadging.js
@@ -1,53 +1,60 @@
-import React from 'react';
-import { Layout } from '../../components';
-import { loginArrow } from '../../assets/images';
+import { useState } from "react";
+import { Layout, Loader } from "../../components";
+import { loginArrow } from "../../assets/images";
 
-import '../../assets/styles/global.scss';
-import './getStartedBadging.scss';
+import "../../assets/styles/global.scss";
+import "./getStartedBadging.scss";
 
 const GetStartedBadging = () => {
-  const baseurl = 'https://badging.allinopensource.org/api';
+	const [openLoader, setOpenLoader] = useState(false);
 
-  const handleLogin = () => {
-    window.location.href = `${baseurl}/login`;
-  };
-  return (
-    <Layout>
-      <div className="section-two">
-        <div className="section-wrap">
-          <div className="section-title">
-            <h2>Prerequisite</h2>
-          </div>
-          <ol className="section-list">
-            <li>
-              A repository is required within which to put the DEI.md file
-            </li>
-            <li>
-              The person applying for the badge must be a project admin or
-              maintainer
-            </li>
+	const baseurl = "https://badging.allinopensource.org/api";
 
-            <li>The Project must be Open Source</li>
-            <li>Ensure you have your DEI.md File</li>
-          </ol>
+	const handleLogin = () => {
+		setOpenLoader(true);
+		window.location.href = `${baseurl}/login`;
+	};
 
-          <div className="login-github">
-            <button
-              type="button"
-              className="login-github"
-              onClick={handleLogin}
-            >
-              <span className="btn-mb-badge">Get Started</span>{' '}
-              <span className="btn-dt-badge"> Login with GitHub</span>
-              <span>
-                <img className="arrow" src={loginArrow} alt="arrow icon" />
-              </span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<div className="section-two">
+				<div className="section-wrap">
+					<div className="section-title">
+						<h2>Prerequisite</h2>
+					</div>
+					<ol className="section-list">
+						<li>
+							A repository is required within which to put the DEI.md file
+						</li>
+						<li>
+							The person applying for the badge must be a project admin or
+							maintainer
+						</li>
+
+						<li>The Project must be Open Source</li>
+						<li>Ensure you have your DEI.md File</li>
+					</ol>
+
+					<div className="login-github">
+						<button
+							type="button"
+							className="login-github"
+							onClick={handleLogin}
+						>
+							<span className="btn-mb-badge">Get Started</span>{" "}
+							<span className="btn-dt-badge"> Login with GitHub</span>
+							<span>
+								<img className="arrow" src={loginArrow} alt="arrow icon" />
+							</span>
+						</button>
+					</div>
+				</div>
+			</div>
+			<Loader open={openLoader}>
+				<p>Redirecting to GitHub Authentication</p>
+			</Loader>
+		</Layout>
+	);
 };
 
 export default GetStartedBadging;
