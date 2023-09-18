c9257a01b6dbfa069e36599be4b4b00a7ee43b98
mide358 <iyimide.adegunloye@gmail.com>
2023-09-18 22:07:09 +0100

    home page updates

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,9 +9,9 @@ import {
 } from '../../components';
 
 import { faqData } from '../../components/Faq/data';
-import { featuredProjects, howItWorksData } from './data';
-import { heroImage, howItWorks } from '../../assets/images';
-import zIndex from '@mui/material/styles/zIndex';
+import { howItWorksData } from './data';
+import { howItWorks } from '../../assets/images';
+// import zIndex from '@mui/material/styles/zIndex';
 import { useMediaQuery } from 'react-responsive';
 // import "../../assets/images/others/bg-how-it-works-web.png"
 
@@ -20,40 +20,37 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <div className="sub-nav" 
-      style={{ 
-        background:`url(${howItWorks}) top center no-repeat`, 
+      <div
+        className="sub-nav"
+        style={{
+          background: `url(${howItWorks}) top center no-repeat`,
         }}
-        >
+      >
         <div className="hero-section">
           <div className="title">DEI Project Badging</div>
           <div className="parra">
             <div className="description">
               <div className="shape"></div>
-              Join us on our mission to recognize the exemplary DEI efforts
-              within open-source projects and communities. Whether you have
-              implemented a welcoming environment or fostered a diverse and
-              inclusive practice, we want to celebrate your achievements and
-              recognize the DEI efforts within your open-source projects. Take
-              the steps towards prioritizing and advancing DEI within your
+              Take a step towards prioritizing and advancing DEI within your
               project and the broader open-source community.
             </div>
           </div>
         </div>
         <CardCarousel />
       </div>
-      <div className="how-it-works" 
-      style={{ 
-        backgroundImage:`url(${!isMobile ? '' : howItWorks})`, 
-        height:"100%", 
-        width:'100%',
-        backgroundRepeat:'no-repeat',
-        backgroundSize:'cover',
-        backgroundPosition:'center'
-        // position:'absolute'
+      <div
+        className="how-it-works"
+        style={{
+          backgroundImage: `url(${!isMobile ? '' : howItWorks})`,
+          height: '100%',
+          width: '100%',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          // position:'absolute'
         }}
-        >
-        <div className="projects wrap">
+      >
+        {/* <div className="projects wrap">
           <p className="text-lg">
             Join 150+ open source projects trusting us to badge their projects.
           </p>
@@ -64,19 +61,15 @@ const Home = () => {
               </div>
             ))}
           </div>
-        </div>
+        </div> */}
         <div className="content-wrap wrap">
           <div className="content">
             <div>
               <h2>How it Works</h2>
               <p className="text-lg">
-              With the help of a DEI.md file, we can delve into a project's DEI 
-              effort and highlight the maintainers' focus on CHAOSS's DEI metrics. 
-              The file will enable us to assess and validate a project's DEI efforts 
-              based on Project Access, Communication Transparency, Newcomer Experience, 
-              and Inclusive Leadership metrics. We provide comprehensive reports and 
-              insight to maintainers to enable them to reflect on and address the 
-              requirements to foster more diverse and healthy open-source communities.
+                Through the use of a DEI.md markdown document, open-source
+                maintainers can actively demonstrate their ongoing commitment to
+                promoting
               </p>
             </div>
             <div className="cards">
@@ -95,27 +88,28 @@ const Home = () => {
           </div>
         </div>
       </div>
-      {/* <div className="main" style={{ 
-        backgroundImage:`url(${heroImage})`, 
-        height:"500px", 
+      {/* <div className="main" style={{
+        backgroundImage:`url(${heroImage})`,
+        height:"500px",
         width:'100%',
         backgroundRepeat:'no-repeat',
         }}>
-        hello 
+        hello
       </div> */}
       <section className="container">
-        <div className="faq"
-         style={{ 
-          background:`url(${howItWorks}) center top no-repeat`, 
+        <div
+          className="faq"
+          style={{
+            background: `url(${howItWorks}) center top no-repeat`,
           }}
         >
-          <h2>Still Have Questions</h2>
+          <h2>Still Have Questions?</h2>
           {faqData.map(({ question, answer }, id) => {
             return <Faq key={id} question={question} answer={answer} />;
           })}
         </div>
       </section>
-
+      <hr className="reed" />
       <Footer />
     </div>
   );
