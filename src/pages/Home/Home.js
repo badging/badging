d52123b2d088704124f3fa1810a55ccbd200761a
Precious Abubakar <preciousdanabubakar@gmail.com>
2023-09-20 12:11:46 +0100

    fix: edit home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,56 +1,56 @@
-import '../../assets/styles/global.scss';
-import './home.scss';
+import "../../assets/styles/global.scss";
+import "./home.scss";
 import {
-  Footer,
-  Header,
-  Faq,
-  CardComponent,
-  CardCarousel,
-} from '../../components';
+	Footer,
+	Header,
+	Faq,
+	CardComponent,
+	CardCarousel,
+} from "../../components";
 
-import { faqData } from '../../components/Faq/data';
-import { howItWorksData } from './data';
-import { howItWorks } from '../../assets/images';
+import { faqData } from "../../components/Faq/data";
+import { howItWorksData } from "./data";
+import { howItWorks, howItWorksBgWeb } from "../../assets/images";
 // import zIndex from '@mui/material/styles/zIndex';
-import { useMediaQuery } from 'react-responsive';
+import { useMediaQuery } from "react-responsive";
 // import "../../assets/images/others/bg-how-it-works-web.png"
 
 const Home = () => {
-  const isMobile = useMediaQuery({ maxWidth: 650 });
-  return (
-    <div>
-      <Header />
-      <div
-        className="sub-nav"
-        style={{
-          background: `url(${howItWorks}) top center no-repeat`,
-        }}
-      >
-        <div className="hero-section">
-          <div className="title">DEI Project Badging</div>
-          <div className="parra">
-            <div className="description">
-              <div className="shape"></div>
-              Take a step towards prioritizing and advancing DEI within your
-              project and the broader open-source community.
-            </div>
-          </div>
-        </div>
-        <CardCarousel />
-      </div>
-      <div
-        className="how-it-works"
-        style={{
-          backgroundImage: `url(${!isMobile ? '' : howItWorks})`,
-          height: '100%',
-          width: '100%',
-          backgroundRepeat: 'no-repeat',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          // position:'absolute'
-        }}
-      >
-        {/* <div className="projects wrap">
+	const isMobile = useMediaQuery({ maxWidth: 650 });
+	return (
+		<div>
+			<Header />
+			<div
+				className="sub-nav"
+				style={{
+					background: `url(${howItWorks}) top center no-repeat`,
+				}}
+			>
+				<div className="hero-section">
+					<div className="title">DEI Project Badging</div>
+					<div className="parra">
+						<div className="description">
+							<div className="shape"></div>
+							Take a step towards prioritizing and advancing DEI within your
+							project and the broader open-source community.
+						</div>
+					</div>
+				</div>
+				<CardCarousel />
+			</div>
+			<div
+				className="how-it-works"
+				// style={{
+				// 	backgroundImage: `url(${!isMobile ? "" : howItWorks})`,
+				// 	height: "100%",
+				// 	width: "100%",
+				// 	backgroundRepeat: "no-repeat",
+				// 	backgroundSize: "cover",
+				// 	backgroundPosition: "center",
+				// 	// position:'absolute'
+				// }}
+			>
+				{/* <div className="projects wrap">
           <p className="text-lg">
             Join 150+ open source projects trusting us to badge their projects.
           </p>
@@ -62,33 +62,40 @@ const Home = () => {
             ))}
           </div>
         </div> */}
-        <div className="content-wrap wrap">
-          <div className="content">
-            <div>
-              <h2>How it Works</h2>
-              <p className="text-lg">
-                Through the use of a DEI.md markdown document, open-source
-                maintainers can actively demonstrate their ongoing commitment to
-                promoting
-              </p>
-            </div>
-            <div className="cards">
-              {howItWorksData.map(({ id, title, text, link, icon }) => {
-                return (
-                  <CardComponent
-                    key={id}
-                    title={title}
-                    text={text}
-                    link={link}
-                    icon={icon}
-                  />
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* <div className="main" style={{
+				<div
+					className="content-wrap wrap"
+					style={{
+						background: `url(${
+							isMobile ? howItWorks : howItWorksBgWeb
+						}) center top no-repeat`,
+					}}
+				>
+					<div className="content">
+						<div>
+							<h2>How it Works</h2>
+							<p className="text-lg">
+								Through the use of a DEI.md markdown document, open-source
+								maintainers can actively demonstrate their ongoing commitment to
+								promoting
+							</p>
+						</div>
+						<div className="cards">
+							{howItWorksData.map(({ id, title, text, link, icon }) => {
+								return (
+									<CardComponent
+										key={id}
+										title={title}
+										text={text}
+										link={link}
+										icon={icon}
+									/>
+								);
+							})}
+						</div>
+					</div>
+				</div>
+			</div>
+			{/* <div className="main" style={{
         backgroundImage:`url(${heroImage})`,
         height:"500px",
         width:'100%',
@@ -96,23 +103,23 @@ const Home = () => {
         }}>
         hello
       </div> */}
-      <section className="container">
-        <div
-          className="faq"
-          style={{
-            background: `url(${howItWorks}) center top no-repeat`,
-          }}
-        >
-          <h2>Still Have Questions?</h2>
-          {faqData.map(({ question, answer }, id) => {
-            return <Faq key={id} question={question} answer={answer} />;
-          })}
-        </div>
-      </section>
-      <hr className="reed" />
-      <Footer />
-    </div>
-  );
+			<section className="container">
+				<div
+					className="faq"
+					style={{
+						background: `url(${howItWorks}) center top no-repeat`,
+					}}
+				>
+					<h2>Still Have Questions?</h2>
+					{faqData.map(({ question, answer }, id) => {
+						return <Faq key={id} question={question} answer={answer} />;
+					})}
+				</div>
+			</section>
+			<hr className="reed" />
+			<Footer />
+		</div>
+	);
 };
 
 export default Home;
