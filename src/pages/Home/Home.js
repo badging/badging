5a06b114dbf342e7d33b191d29af8143c85c8143
mide358 <iyimide.adegunloye@gmail.com>
2023-10-02 21:35:02 +0100

    fixed get started badging alignment

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,109 +1,87 @@
-import "../../assets/styles/global.scss";
-import "./home.scss";
+import '../../assets/styles/global.scss';
+import './home.scss';
 import {
-	Footer,
-	Header,
-	Faq,
-	CardComponent,
-	CardCarousel,
-} from "../../components";
+  Footer,
+  Header,
+  Faq,
+  CardComponent,
+  CardCarousel,
+} from '../../components';
 
-import { faqData } from "../../components/Faq/data";
-import { howItWorksData } from "./data";
+import { faqData } from '../../components/Faq/data';
+import { howItWorksData } from './data';
 import {
-	howItWorksBgMobile,
-	howItWorksBgWeb,
-	faqBgWeb,
-	faqBgMobile,
-	heroSectionBgWeb,
-	heroSectionBgMobile,
-} from "../../assets/images";
+  howItWorksBgMobile,
+  howItWorksBgWeb,
+  faqBgWeb,
+  faqBgMobile,
+  heroSectionBgWeb,
+  heroSectionBgMobile,
+} from '../../assets/images';
 // import zIndex from '@mui/material/styles/zIndex';
-import { useMediaQuery } from "react-responsive";
+import { useMediaQuery } from 'react-responsive';
 
 const Home = () => {
-	const isMobile = useMediaQuery({ maxWidth: 650 });
-	return (
-		<div>
-			<Header />
-			<div
-				className="sub-nav"
-				style={{
-					background: `url(${
-						isMobile ? heroSectionBgMobile : heroSectionBgWeb
-					}) center top no-repeat`,
-				}}
-			>
-				<div className="hero-section">
-					<div className="title">DEI Project Badging</div>
-					<div className="parra">
-						<div className="description">
-							<div className="shape"></div>
-							Take a step towards prioritizing and advancing DEI within your
-							project and the broader open-source community.
-						</div>
-					</div>
-				</div>
-				<CardCarousel />
-			</div>
-			<div
-				className="how-it-works"
-				// style={{
-				// 	backgroundImage: `url(${!isMobile ? "" : howItWorks})`,
-				// 	height: "100%",
-				// 	width: "100%",
-				// 	backgroundRepeat: "no-repeat",
-				// 	backgroundSize: "cover",
-				// 	backgroundPosition: "center",
-				// 	// position:'absolute'
-				// }}
-			>
-				{/* <div className="projects wrap">
-          <p className="text-lg">
-            Join 150+ open source projects trusting us to badge their projects.
-          </p>
-          <div className="logos">
-            {featuredProjects.map((project, index) => (
-              <div className="img-wrap" key={index}>
-                <img src={project.logo} alt={`${project.name} logo`} />
-              </div>
-            ))}
+  const isMobile = useMediaQuery({ maxWidth: 650 });
+  return (
+    <div>
+      <Header />
+      <div
+        className="sub-nav"
+        style={{
+          background: `url(${
+            isMobile ? heroSectionBgMobile : heroSectionBgWeb
+          }) center top no-repeat`,
+        }}
+      >
+        <div className="hero-section">
+          <div className="title">DEI Project Badging</div>
+          <div className="parra">
+            <div className="description">
+              <div className="shape"></div>
+              Take a step towards prioritizing and advancing DEI within your
+              project and the broader open-source community.
+            </div>
           </div>
-        </div> */}
-				<div
-					className="content-wrap wrap"
-					style={{
-						background: `url(${
-							isMobile ? howItWorksBgMobile : howItWorksBgWeb
-						}) center top no-repeat`,
-					}}
-				>
-					<div className="content">
-						<div>
-							<h2>How it Works</h2>
-							<p className="text-lg">
-								Through the use of a DEI.md markdown document, open-source
-								maintainers can actively demonstrate their ongoing commitment to
-								promoting
-							</p>
-						</div>
-						<div className="cards">
-							{howItWorksData.map(({ id, title, text, link, icon }) => {
-								return (
-									<CardComponent
-										key={id}
-										title={title}
-										text={text}
-										link={link}
-										icon={icon}
-									/>
-								);
-							})}
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* <div className="main" style={{
+        </div>
+        <CardCarousel />
+      </div>
+
+      <div className="how-it-works">
+        <div
+          className="content-wrap wrap"
+          style={{
+            background: `url(${
+              isMobile ? howItWorksBgMobile : howItWorksBgWeb
+            }) center top no-repeat`,
+          }}
+        >
+          <div className="content">
+            <div>
+              <h2>How it Works</h2>
+              <p className="text-lg">
+                Through the use of a DEI.md markdown document, open-source
+                maintainers can actively demonstrate their ongoing commitment to
+                promoting
+              </p>
+            </div>
+            <div className="cards">
+              {howItWorksData.map(({ id, title, text, link, icon }) => {
+                return (
+                  <CardComponent
+                    key={id}
+                    title={title}
+                    text={text}
+                    link={link}
+                    icon={icon}
+                  />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="main" style={{
         backgroundImage:`url(${heroImage})`,
         height:"500px",
         width:'100%',
@@ -111,28 +89,28 @@ const Home = () => {
         }}>
         hello
       </div> */}
-			<section
-				className="container"
-				style={{
-					background: `url(${!isMobile && faqBgWeb}) center top no-repeat`,
-				}}
-			>
-				<div
-					className="faq"
-					style={{
-						background: `url(${isMobile && faqBgMobile}) center top no-repeat`,
-					}}
-				>
-					<h2>Still Have Questions?</h2>
-					{faqData.map(({ question, answer }, id) => {
-						return <Faq key={id} question={question} answer={answer} />;
-					})}
-				</div>
-			</section>
-			<hr className="reed" />
-			<Footer />
-		</div>
-	);
+      <section
+        className="container"
+        style={{
+          background: `url(${!isMobile && faqBgWeb}) center top no-repeat`,
+        }}
+      >
+        <div
+          className="faq"
+          style={{
+            background: `url(${isMobile && faqBgMobile}) center top no-repeat`,
+          }}
+        >
+          <h2>Still Have Questions?</h2>
+          {faqData.map(({ question, answer }, id) => {
+            return <Faq key={id} question={question} answer={answer} />;
+          })}
+        </div>
+      </section>
+      <hr className="reed" />
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
