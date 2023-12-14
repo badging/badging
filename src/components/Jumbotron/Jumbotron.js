f086c5881f6344b6b2d184a73fdda890840e84eb
Tochukwu <tochukwuodoeme@gmail.com>
2023-12-14 21:42:16 +0100

    add background

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,38 +2,31 @@ import React from "react";
 import PropTypes from "prop-types";
 import "../../assets/styles/global.scss";
 import "./jumbotron.scss";
-const Jumbotron = ({
-  title,
-  description,
-  img,
-  subheading = null,
-  subtitle,
-  children,
-}) => {
+import { Header } from "..";
+
+const Jumbotron = () => {
   return (
-    <div className="jumbotron">
-      <div className="jumbo-main">
-        {img} <h1>{title}</h1>
-        <p className="sub">{subtitle}</p>
+    <>
+      <main>
+        <div className='overlay'>
+            <div className='bgRight'></div>
+            <div className='bgCenter'></div>
+            <div className='bgLeft'></div>
+        </div>
+      <Header />
+      <div className="container jumbotron__container">
+      
+        {/* <img src={curlyBraces} alt="badging-logo" /> */}
+        <h1>Event Badging</h1>
+        <div className="about-project">
+          <button onClick={() =>swapHandler('about')} className={swap == 'about' ? 'buttonActive' : 'buttonInActive'}>About Project Badging</button>
+          <a href="/about-project-badging">Badged Projects</a>
+          {/* <button onClick={() => swapHandler('project')} className={swap == 'project' ? 'buttonActive' : 'buttonInActive'}>Badged Projects</button> */}
+        </div>
       </div>
-
-      <div>
-        {subheading && <h3>{subheading}</h3>}
-        <p>{description}</p>
-      </div>
-      {/* add any other children */}
-      {children}
-    </div>
+      </main>
+    </>
   );
-};
-
-Jumbotron.propTypes = {
-  title: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  subheading: PropTypes.string,
-  children: PropTypes.node,
-  img: PropTypes.any,
-  subtitle: PropTypes.string,
 };
 
 export default Jumbotron;
