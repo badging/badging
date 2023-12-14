b8aa13ce9c193e920594809057652d40762c330f
Tochukwu <tochukwuodoeme@gmail.com>
2023-12-14 21:10:00 +0100

    add mobile nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { chaossLogo, mobileMenuIcon } from '../../assets/images'
 import './header.scss';
 import { useLocation } from 'react-router-dom';
@@ -6,9 +6,14 @@ import { useLocation } from 'react-router-dom';
 const Nav = () => {
   const location = useLocation();
   const pathname = location.pathname;
+  const [toggleNav, setToggleNav] = useState(false)
+
+
 
   return (
-    <header className='container-new'>
+    <div>
+
+      <header className='container-new'>
         <img src={chaossLogo} alt="logo" className='logo'/>
         <ul>
           <li><a href="/" className={pathname == '/'? 'activeNav': ''}>Home</a></li>
@@ -16,7 +21,7 @@ const Nav = () => {
           <li><a href="/about-project-badging" className={pathname == '/about-project-badging'? 'activeNav': ''}>Project Badging</a></li>
           <li><a href="/">FAQ</a></li>
         </ul>
-        <img className='mobileNav' src={mobileMenuIcon} alt="logo" />
+        <img className='mobileNav' onClick={() => setToggleNav(!toggleNav)} src={mobileMenuIcon} alt="logo" />
         {
           pathname && pathname !== '/' ? (
             <a href='/' className='apply-btn'>Apply for Badging</a>
@@ -24,8 +29,17 @@ const Nav = () => {
             <div className='hidden-box'></div>
           )
         }
-        
      </header>
+     <div className="mobileNavContainer" style={{display: toggleNav ==true ? '' : 'none'}}>
+          <ul className='mobile-list'>
+            <li><a href="/" className={pathname == '/'? 'activeNav': ''}>Home</a></li>
+            <li><a href="/event-badging" className={pathname == '/event-badging'? 'activeNav': ''}>Event Badging</a></li>
+            <li><a href="/about-project-badging" className={pathname == '/about-project-badging'? 'activeNav': ''}>Project Badging</a></li>
+            <li><a href="/">FAQ</a></li>
+          </ul>
+        </div>
+    </div>
+    
   )
 }
 
