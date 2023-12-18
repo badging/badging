96ade00b03a62ced3118b6c27ab20e4fc7ca8088
Matt Germonprez <germonprez@gmail.com>
2023-12-18 16:01:10 -0600

    Update DeiFile.js
    Updating the text for this page

--- src/components/AboutDeiBadging/DeiFile.js
+++ src/components/AboutDeiBadging/DeiFile.js
@@ -5,15 +5,15 @@ const DeiFile = () => {
     <div className="dei__file ">
       <h2 id="dei-file">DEI.md File</h2>
       <p>
-        The DEI.md file is an essential component of the All In CHAOSS DEI
-        badging initiative. It serves as a benchmark for open source projects to
+        The DEI.md file is an essential component of CHAOSS DEI Project
+        Badging. It serves as a benchmark for open source projects to
         openly acknowledge and showcase their dedication to diversity, equity,
         and inclusion (DEI).
       </p>
 
       <p>
-        The DEI.md file can be used by maintainers to explicitly express and
-        self-reflect on their project&#39;s approach to DEI. Maintainers are
+        The DEI.md file can be used to explicitly express and
+        self-reflect on their project&#39;s approach to DEI. Community members are
         encouraged to include relevant content that reflects their project&#39;s
         specific approaches and initiatives regarding diversity, equity, and
         inclusion within the DEI.md file. This may include outlining strategies,
@@ -32,7 +32,7 @@ const DeiFile = () => {
           file in a community repository
         </li>
         <li>
-          Use the following <a href="##">DEI.md template</a> to develop your
+          Use the following <a href="https://github.com/badging/badging/blob/main/Template.DEI.md">DEI.md template</a> and <a href="https://github.com/badging/badging/blob/main/Guide.DEI.md">DEI.md guide</a> to develop your
           project DEI.md file.
         </li>
       </ul>
