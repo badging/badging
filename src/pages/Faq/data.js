94a63c610280c61e9b025909698dc8be57a32e53
Precious Abubakar <preciousdanabubakar@gmail.com>
2024-01-19 07:16:11 +0100

    fx: create clickable links for each FAQ for easy sharing, Closes #54, Signed-off-by: Precious Abubakar <preciousdanabubakar@gmail.com>

---
+++ src/pages/Faq/data.js
@@ -0,0 +1,104 @@
+export const eventFaqs = [
+  {
+    id: 1,
+    title:
+      "What requirements are needed before I apply for a CHAOSS DEI Event Badge?",
+    description:
+      "To submit an application for your event/conference, you will need to have a GitHub account, as well as the information listed in the Event Badging section of the site.",
+  },
+  {
+    id: 2,
+    title: "Why do I need a CHAOSS DEI Event Badge?",
+    description:
+      "Events, projects, and organizations can apply for the DEI badge to help measure and demonstrate their commitment to DEI values and practices and inspire others to follow in their footsteps.",
+  },
+  {
+    id: 3,
+    title: "What happens if my event is awarded a 'Pending' badge?",
+    description:
+      "Pending events have an opportunity to improve their badge level by altering their processes and procedures and improving the information that is available on the event website. This is a partnership with CHAOSS and event organizers can work with the badge reviewers on ideas for improvement.",
+  },
+  {
+    id: 4,
+    title:
+      "If my event happens every year, can I use the badge from past events?",
+    description:
+      "No. We ask that you submit a new application for every separate event. We will occasionally be changing the metrics used in the application, and in some cases, there will be different questions on the application from year to year.",
+  },
+  {
+    id: 5,
+    title: "Where can I ask questions about Event Badging?",
+    description: (
+      <>
+        If you have quesitons about Event Badging, please either{" "}
+        <a
+          href="https://github.com/badging/badging"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open an issue in our badging repo
+        </a>{" "}
+        or reach us at the{" "}
+        <a
+          href="https://join.slack.com/t/chaoss-workspace/shared_invite/zt-28p56bayt-67TRjdA4yJWQmUd4hCzULg"
+          target="_blank"
+          rel="noreferrer"
+        >
+          community on Slack
+        </a>
+      </>
+    ),
+  },
+];
+
+export const projectFaqs = [
+  {
+    id: 1,
+    title:
+      "What are the requirements needed to apply for a CHAOSS DEI Project Badge?",
+    description:
+      "To apply for a badge, you will need a DEI.md file that is publicly available. You can learn more about this file in the Project Badging section of this site.",
+  },
+  {
+    id: 2,
+    title: "How do I get a badge?",
+    description:
+      "To obtain a badge, simply click the 'Apply for a Badge button at the top of the site, then follow the directions from there.'",
+  },
+  {
+    id: 3,
+    title: "How frequently can I apply for a badge?",
+    description:
+      "You can apply for the project badge for any open-source project or organization annually.",
+  },
+  {
+    id: 4,
+    title: "Where can I ask questions about Project Badging?",
+    description: (
+      <>
+        If you have quesitons about Project Badging, please either{" "}
+        <a
+          href="https://github.com/badging/badging"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open an issue in our badging repo
+        </a>{" "}
+        or reach us at the{" "}
+        <a
+          href="https://join.slack.com/t/chaoss-workspace/shared_invite/zt-28p56bayt-67TRjdA4yJWQmUd4hCzULg"
+          target="_blank"
+          rel="noreferrer"
+        >
+          community on Slack
+        </a>
+      </>
+    ),
+  },
+  {
+    id: 5,
+    title: "What if I'm not seeing my repository when applying for a badge?",
+    description:
+      "If you aren't seeing your repository during the application process, try clearning out your cache or trying a different browser. If that doesn't help [open an issue in our badging repo](https://github.com/badging/badging) or reach on the [community on Slack](https://join.slack.com/t/chaoss-workspace/shared_invite/zt-28p56bayt-67TRjdA4yJWQmUd4hCzULg)",
+  },
+];
