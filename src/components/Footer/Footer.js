08321824617276fddd93a1138be05f5919784c1d
Precious Abubakar <preciousdanabubakar@gmail.com>
2024-01-19 02:35:17 +0100

    ft: Change the email icon to slack in the footer and link it to the badging slack channel, Closes #50, Signed-off-by: Precious Abubakar <preciousdanabubakar@gmail.com>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,35 +1,34 @@
-import React from 'react'
-// import { githubIcon, linkedin, mail, twitter } from '../../../assets/images'
-import './footer.scss';
-import { Link } from 'react-router-dom';
-import { githubIcon, linkedin, mail, twitter } from '../../assets/images';
+import React from "react";
+import "./footer.scss";
+import { Link } from "react-router-dom";
+import { githubIcon, linkedin, twitter, slack } from "../../assets/images";
 
 const Footer = () => {
   return (
-    <footer className='footer-container'>
-        <span className='social'>
-        <Link
-          to="https://github.com/badging/badging"
-          target="blank"
-        >
+    <footer className="footer-container">
+      <span className="social">
+        <Link to="https://github.com/badging/badging" target="blank">
           <img src={githubIcon} alt="github-icon" />
         </Link>
-        <Link
-          to="https://www.linkedin.com/company/chaoss/"
-          target="blank"
-        >
+        <Link to="https://www.linkedin.com/company/chaoss/" target="blank">
           <img src={linkedin} alt="linkedin-icon" />
         </Link>
         <Link to="https://twitter.com/chaossproj" target="blank">
           <img src={twitter} alt="twitter-icon" />
         </Link>
-        <Link to="mailto:chaossbadge@gmail.com" target="blank">
-          <img src={mail} alt="mail-icon" />
+        <Link
+          to="https://chaoss-workspace.slack.com/messages/C012A4L7M8V/"
+          target="blank"
+        >
+          <img src={slack} alt="slack-icon" />
         </Link>
-        </span>
-        <span className='copyright'>Copyright &copy; <a href="/">CHAOSS.</a> All rights reserved a Linux Foundation Project</span>
+      </span>
+      <span className="copyright">
+        Copyright &copy; <a href="/">CHAOSS.</a> All rights reserved a Linux
+        Foundation Project
+      </span>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
