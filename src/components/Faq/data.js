7c574f3b8cfa82db3b2ba9ca03ec6919efbd79c3
mide358 <iyimide.adegunloye@gmail.com>
2024-02-03 13:58:01 +0000

    rebased refactored faq page
    Signed-off-by: mide358 <iyimide.adegunloye@gmail.com>

---
+++ src/components/Faq/data.js
@@ -0,0 +1,14 @@
+export const faqData = [
+  {
+    question: `What is CHAOSS?`,
+    answer: `Lorem ipsum dolor sit amet consectetur. Fringilla amet sed urna vel. Ultrices platea orci nisl nisl mattis nunc. Venenatis blandit eget eu gravida porta. Fames diam justo purus pellentesque ante velit platea faucibus et. Quam posuere risus sit pellentesque cursus quam. Neque eget mi dolor euismod. `,
+  },
+  {
+    question: `Why DEI Badging?`,
+    answer: `Lorem ipsum dolor sit amet consectetur. Fringilla amet sed urna vel. Ultrices platea orci nisl nisl mattis nunc. Venenatis blandit eget eu gravida porta. Fames diam justo purus pellentesque ante velit platea faucibus et. Quam posuere risus sit pellentesque cursus quam. Neque eget mi dolor euismod. `,
+  },
+  {
+    question: 'What DEI Badging?',
+    answer: `Lorem ipsum dolor sit amet consectetur. Fringilla amet sed urna vel. Ultrices platea orci nisl nisl mattis nunc. Venenatis blandit eget eu gravida porta. Fames diam justo purus pellentesque ante velit platea faucibus et. Quam posuere risus sit pellentesque cursus quam. Neque eget mi dolor euismod. `,
+  },
+];
--- src/components/index.js
+++ src/components/index.js
@@ -1,20 +1,21 @@
-import Header from "./Header/Header";
-import Footer from "./Footer/Footer";
-import Faq from "./Faq/Faq";
-import CardComponent from "./HowItWorks/Card";
-import CardCarousel from "./Carousel/CardCarousel";
-import SearchBar from "./SelectProject/SearchBar";
-import Sidebar from "./AboutDeiBadging/Sidebar";
-import Jumbotron from "./Jumbotron/Jumbotron";
-import AboutDeiMobile from "./AboutDeiBadging/AboutDeiMobile";
-import WhatIsDeiBadging from "./AboutDeiBadging/WhatIsDeiBadging";
-import ApplicationProcess from "./AboutDeiBadging/ApplicationProcess";
-import DeiFile from "./AboutDeiBadging/DeiFile";
-import DeiBadge from "./AboutDeiBadging/DeiBadge";
-import SelectedProjects from "./SelectProject/SelectedProjects";
-import Layout from "./Layout/Layout";
-import Spinner from "./loader/Spinner";
-import Loader from "./loader/Loader";
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Faq from './Faq/Faq';
+import CardComponent from './HowItWorks/Card';
+import CardCarousel from './Carousel/CardCarousel';
+import SearchBar from './SelectProject/SearchBar';
+import Sidebar from './AboutDeiBadging/Sidebar';
+import Jumbotron from './Jumbotron/Jumbotron';
+import FaqSidebar from './Faq/FaqSidebar';
+import AboutDeiMobile from './AboutDeiBadging/AboutDeiMobile';
+import WhatIsDeiBadging from './AboutDeiBadging/WhatIsDeiBadging';
+import ApplicationProcess from './AboutDeiBadging/ApplicationProcess';
+import DeiFile from './AboutDeiBadging/DeiFile';
+import DeiBadge from './AboutDeiBadging/DeiBadge';
+import SelectedProjects from './SelectProject/SelectedProjects';
+import Layout from './Layout/Layout';
+import Spinner from './loader/Spinner';
+import Loader from './loader/Loader';
 
 export {
   Header,
@@ -25,6 +26,7 @@ export {
   Jumbotron,
   SearchBar,
   Sidebar,
+  FaqSidebar,
   AboutDeiMobile,
   WhatIsDeiBadging,
   ApplicationProcess,
